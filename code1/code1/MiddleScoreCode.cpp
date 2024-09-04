203070ec5b248f67746ace1981ffece6da8aabfb
redtea232 <xkdhwjd99@gmail.com>
2024-09-04 21:32:00 +0900

    MiddleScoreCode

--- code1/code1/MiddleScoreCode.cpp
+++ code1/code1/MiddleScoreCode.cpp
@@ -2,8 +2,25 @@
 #include<stdlib.h>
 
 int main(void) {
-	
-	printf("hello world");
 
+	int i, j, tmp;
+
+	int array[5] = {3, 1, 4, 5, 2};
+
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 5; j++) {
+			if (array[i] < array[j]) {
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+			}
+		}
+	}
+
+	for (i = 0; i < 5; i++) {
+		printf("%d ", array[i]);
+	}
 	return 0;
+
+	a = 1 ? 2 : 5;
 }
